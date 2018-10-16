30338951b20514b0f50874190f6d062210ecd95d
JaChudy <jakub.chudy@gmail.com>
2018-10-16 18:30:36 +0200

    book class with Entity

--- src/main/java/pl/chudyja/com/demo/model/Book.java
+++ src/main/java/pl/chudyja/com/demo/model/Book.java
@@ -1,4 +1,27 @@
 package pl.chudyja.com.demo.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String author;
+
+    //nie musumy dodawać geterów i seterów adnotacja @Data z biblioteki Lombok to za nas załatwi
+
+
+    public Book(String title, String isbn, String author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
 }
